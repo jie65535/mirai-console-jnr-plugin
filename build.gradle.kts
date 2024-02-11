a201911817b6fba3abdda552d83e9151932c32aa
枫灵剑影 <2583963775@qq.com>
2024-02-12 01:48:07 +0800

    升级 mirai-console 依赖到 2.16.0；使用 Mirai kdoc 推荐的图片上传方式，修复图片宽高异常的问题。

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.14.0"
+    id("net.mamoe.mirai-console") version "2.16.0"
 }
 
 group = "top.jie65535"
